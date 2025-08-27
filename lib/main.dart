0fb62a72137440a4b74dc143b881bfe072afa527
Mohsin832 <mm8116900@gmail.com>
2025-08-27 10:25:58 -0700

    tictactoe app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:tictactoe/tictactoe.dart';
+import 'package:tictactoe/screens/game.dart';
 
 void main() {
-  runApp(Tictactoe());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Game(),
+    );
+  }
 }
