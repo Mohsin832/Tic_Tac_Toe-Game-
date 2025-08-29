48a4610fa6c7487468b2a8ba028f007a97be34df
Mohsin832 <mm8116900@gmail.com>
2025-08-29 08:36:05 -0700

    tic tac toe app

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -10,23 +10,86 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
+  void tapped(int index) {
+    setState(() {
+      if (oTurn == true && displayXO[index] == "") {
+        displayXO[index] = "O";
+      } else {
+        displayXO[index] = "X";
+      }
+      oTurn = !oTurn;
+    });
+  }
+
+  bool oTurn = true;
+  List<String> displayXO = ["", "", "", "", "", "", "", "", ""];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.primaryColor,
-      body: Column(
-        children: [
-          Expanded(
-            child: Uihelper.customText(
-              text: "Score board",
-              fontsize: 18,
-              context: context,
-              fontWeight: FontWeight.normal,
-              fontfamily: "Regular",
-              letterSpacing: 3,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Uihelper.customText(
+                text: "Score board",
+                fontsize: 18,
+                context: context,
+                fontWeight: FontWeight.normal,
+                fontfamily: "Regular",
+                letterSpacing: 3,
+              ),
             ),
-          ),
-        ],
+            Expanded(
+              flex: 3,
+              child: GridView.builder(
+                itemCount: 9,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                ),
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      tapped(index);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(
+                          width: 5,
+                          color: AppColor.primaryColor,
+                        ),
+                        color: AppColor.secondaryColor,
+                      ),
+                      child: Center(
+                        child: Uihelper.customText(
+                          text: displayXO[index],
+                          fontsize: 80,
+                          context: context,
+                          fontWeight: FontWeight.bold,
+                          color: AppColor.primaryColor,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Uihelper.customText(
+                text: "Timer",
+                fontsize: 18,
+                context: context,
+                fontWeight: FontWeight.normal,
+                fontfamily: "Regular",
+                letterSpacing: 3,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
